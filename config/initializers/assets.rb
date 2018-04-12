35f5ac75b4edc038b557f75e295595e0a813f757
Steve Chesnowitz <steve@chesnowitz.com>
2018-04-12 13:34:25 -0400

    partials complete

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,19 +9,12 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( pages/bootstrap3/css/bootstrap.min.css )
-
 Rails.application.config.assets.precompile += %w( pages/animate/animate.css)
-
 Rails.application.config.assets.precompile += %w( pages/one-page-parallax/style.css )
-
 Rails.application.config.assets.precompile += %w( pages/one-page-parallax/style-responsive.css )
-
 Rails.application.config.assets.precompile += %w( pages/one-page-parallax/theme/default.css )
-
 Rails.application.config.assets.precompile += %w( pages/pace/pace.min.js )
-
 Rails.application.config.assets.precompile += %w( pages/bootstrap3/js/bootstrap.min.js )
-
 Rails.application.config.assets.precompile += %w( pages/js-cookie/js.cookie.js )
 Rails.application.config.assets.precompile += %w( pages/scrollMonitor/scrollMonitor.js )
 Rails.application.config.assets.precompile += %w( pages/apps.js )
